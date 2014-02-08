b81ef9d79bfab744a08d97b30e51c3dc342011ae
Robert van der Meulen <github@public.wiretrip.org>
2014-02-08 17:40:39 +0100

    Switched from urllib2 to requests

--- Toon.py
+++ Toon.py
@@ -1,11 +1,9 @@
 import re
-import urllib2
-import urllib
-from cookielib import CookieJar
 import json
 import sys
 import uuid
 import pprint
+import requests
 
 """ The Toon module impersonates the Eneco Toon mobile app, and implements
 similar functionality. """
@@ -25,12 +23,9 @@ class Toon:
 		# details.
 		# Agreement details and user details are stored in the
 		# sessiondata variable.
-		self.cj = CookieJar()
-		self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cj))
 		formdata = { "username" : self.username, "password": self.password }
-		data_encoded = urllib.urlencode(formdata)
-		response = self.opener.open("https://toonopafstand.eneco.nl/toonMobileBackendWeb/client/login", data_encoded)
-		self.sessiondata = json.loads(response.read())
+		r = requests.get ("https://toonopafstand.eneco.nl/toonMobileBackendWeb/client/login", params=formdata)
+		self.sessiondata = r.json()
 
 		# Now re-use the agreement details to do the actual
 		# authentication. This establishes a session, and allows
@@ -42,9 +37,8 @@ class Toon:
 				"agreementId": self.sessiondata["agreements"][0]["agreementId"],
 				"agreementIdChecksum": self.sessiondata["agreements"][0]["agreementIdChecksum"],
 				"random": uuid.uuid1() }
-		data_encoded = urllib.urlencode(formdata)
-		url = "https://toonopafstand.eneco.nl/toonMobileBackendWeb/client/auth/start?%s" % data_encoded
-		response = self.opener.open(url)
+		r = requests.get ("https://toonopafstand.eneco.nl/toonMobileBackendWeb/client/auth/start", params=formdata)
+		
 
 	def logout(self):
 		""" Log out of the API. 
@@ -52,9 +46,7 @@ class Toon:
 		formdata = {	"clientId": self.sessiondata["clientId"],
 				"clientIdChecksum": self.sessiondata["clientIdChecksum"],
 				"random": uuid.uuid1() }
-		data_encoded = urllib.urlencode(formdata)
-		url = "https://toonopafstand.eneco.nl/toonMobileBackendWeb/client/auth/logout?%s" % data_encoded
-		response = self.opener.open(url)
+		r = requests.get ("https://toonopafstand.eneco.nl/toonMobileBackendWeb/client/auth/logout", params=formdata)
 		self.toonstate = None
 		self.sessiondata = None
 
@@ -64,10 +56,8 @@ class Toon:
 		formdata = { 	"clientId": self.sessiondata["clientId"],
 				"clientIdChecksum": self.sessiondata["clientIdChecksum"],
 				"random": uuid.uuid1() }
-		data_encoded = urllib.urlencode(formdata)
-		url = "https://toonopafstand.eneco.nl/toonMobileBackendWeb/client/auth/retrieveToonState?%s" % data_encoded
-		response = self.opener.open(url)
-		self.toonstate = json.loads(response.read()) # TODO: check for success
+		r = requests.get("https://toonopafstand.eneco.nl/toonMobileBackendWeb/client/auth/retrieveToonState", params=formdata)
+		self.toonstate = r.json()
 
 	def refreshToonState(self):
 		self.toonstate = None
@@ -95,7 +85,5 @@ class Toon:
                                 "clientIdChecksum": self.sessiondata["clientIdChecksum"],
 				"value": targettemp,
                                 "random": uuid.uuid1() }
-		data_encoded = urllib.urlencode(formdata)
-		url = "https://toonopafstand.eneco.nl/toonMobileBackendWeb/client/auth/setPoint?%s" % data_encoded
-		response = self.opener.open(url)
+		r = requests.get("https://toonopafstand.eneco.nl/toonMobileBackendWeb/client/auth/setPoint", params=formdata)
 
