17995c53c310c3243bec50557f3ae3e8e2c7ec9b
Robert van der Meulen <github@public.wiretrip.org>
2014-02-08 18:00:58 +0100

    pep8 fixes

--- toonclient.py
+++ toonclient.py
@@ -5,32 +5,31 @@ from Toon import Toon
 import argparse
 
 parser = argparse.ArgumentParser(description='Communicate with the Eneco Toon thermostat')
-parser.add_argument('-t',help='return current temperature in Celsius',action='store_true')
-parser.add_argument('-p',help='return current power usage in Watts',action='store_true')
-parser.add_argument('-s','--settemp',help='set target temperature',dest='targettemp')
-parser.add_argument('-U','--username',help='the Toon username',required=True,dest='username')
-parser.add_argument('-P','--password',help='the Toon password',required=True,dest='password')
+parser.add_argument('-t', help='return current temperature in Celsius', action='store_true')
+parser.add_argument('-p', help='return current power usage in Watts', action='store_true')
+parser.add_argument('-s', '--settemp', help='set target temperature', dest='targettemp')
+parser.add_argument('-U', '--username', help='the Toon username', required=True, dest='username')
+parser.add_argument('-P', '--password', help='the Toon password', required=True, dest='password')
 
 args = parser.parse_args()
 
 username = args.username
 password = args.password
 
-toon = Toon(username,password)
+toon = Toon(username, password)
 toon.login()
 
 if args.t:
-	thermostat = toon.get_thermostat_info()
-	temp = float(thermostat["currentTemp"]) / 100
-	print "current_temp:%.2f" % temp
+        thermostat = toon.get_thermostat_info()
+        temp = float(thermostat["currentTemp"]) / 100
+        print "current_temp:%.2f" % temp
 
 if args.p:
-	power = toon.get_power_usage()
-	print "current_powerusage:%d" % power["value"]
+        power = toon.get_power_usage()
+        print "current_powerusage:%d" % power["value"]
 
-if args.targettemp != None:
-	print "set_temp:%s" % args.targettemp
-	toon.set_thermostat(args.targettemp)
-	
+if args.targettemp is not None:
+        print "set_temp:%s" % args.targettemp
+        toon.set_thermostat(args.targettemp)
+
 toon.logout()
-
